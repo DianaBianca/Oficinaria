3f4a0a39d64070b511fac4e8a17f78a86bd23eee
DianaBianca <dianabiancasr@gmail.com>
2024-07-21 14:36:32 -0300

    refactoring controller actions

--- app/controllers/zip_codes_controller.rb
+++ app/controllers/zip_codes_controller.rb
@@ -1,20 +1,25 @@
 class ZipCodesController < ApplicationController
+  before_action :fetch_address_info, only: :search
+
   def index
     @address_info = nil
   end
 
   def search
-    zip_code = params[:cep]
-
-    response = Faraday.get("https://cep.awesomeapi.com.br/json/#{zip_code}")
-
-    @address_info = JSON.parse(response.body, symbolize_names: true)
-    @address_info[:status] = response.status
-
     respond_to do |format|
       format.turbo_stream
       format.html { render :index }
     end
   end
 
+  private
+
+  def fetch_address_info
+    zip_code = params[:cep]
+
+    response = Faraday.get("https://cep.awesomeapi.com.br/json/#{zip_code}")
+
+    @address_info = JSON.parse(response.body, symbolize_names: true)
+    @address_info[:status] = response.status
+  end
 end
